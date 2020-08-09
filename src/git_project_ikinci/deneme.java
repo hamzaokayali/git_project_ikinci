fd4d2f5e32a4f7b68e4cf307dbc95f024fa8d640
hamza <hamzakayali10@gmail.com>
2020-08-09 23:11:48 +0300

    yeni satır

--- src/git_project_ikinci/deneme.java
+++ src/git_project_ikinci/deneme.java
@@ -3,7 +3,8 @@ package git_project_ikinci;
 public class deneme {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("denememem");
+		System.out.println("bakbakbak");
 
 	}
 
